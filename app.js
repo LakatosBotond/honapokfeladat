812ab31c5e8e9bbccbfd6afdd50dfefed56a0ce7
LakatosBotond <lakatosbotond@turr.hu>
2026-09-16 11:05:09 +0200

    index + app js létrehozva, adatok beolvasva tömbbe objektumkent

---
+++ app.js
@@ -0,0 +1,14 @@
+let honapok = [
+    {id: 1 , honapnev: "január" , evszak: "tél" , napokszama:30},
+    {id: 2 , honapnev: "február" , evszak: "tél" , napokszama:30},
+    {id: 3 , honapnev: "március" , evszak: "tavasz" , napokszama:30},
+    {id: 4 , honapnev: "április" , evszak: "tavasz" , napokszama:30},
+    {id: 5 , honapnev: "május" , evszak: "tavasz" , napokszama:30},
+    {id: 6 , honapnev: "junius" , evszak: "nyár" , napokszama:30},
+    {id: 7 , honapnev: "julius" , evszak: "nyár" , napokszama:30},
+    {id: 8 , honapnev: "augusztus" , evszak: "nyár" , napokszama:30},
+    {id: 9 , honapnev: "szeptember" , evszak: "ösz" , napokszama:30},
+    {id: 10 , honapnev: "oktober" , evszak: "ösz" , napokszama:30},
+    {id: 11 , honapnev: "november" , evszak: "ösz" , napokszama:30},
+    {id: 12 , honapnev: "december" , evszak: "tél" , napokszama:30}
+]
